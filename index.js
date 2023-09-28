d5597754c65cf80940b31b838227b77f202aba5a
Lankey Laxmi Sri Suraj <21pa1a1263@vishnu.edu.in>
2023-09-29 00:50:32 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,105 @@
+var express = require('express')
+var app = express()
+var ejs = require('ejs');
+var passwordHash = require('password-hash');
+const v = require('body-parser');
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(v.urlencoded({ extended: true }))
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
+var serviceAccount = require("./key.json");
+
+initializeApp({
+  credential: cert(serviceAccount)
+});
+const db = getFirestore();
+
+app.get('/', function(req, res) {
+  res.render(__dirname + "/views/" + "signup.ejs", { data1: "" });
+
+})
+app.get('/login', function(req, res) {
+  res.render(__dirname + "/views/" + "login.ejs", { data2: "" });
+
+})
+
+app.get('/dashboard', (req, res) => {
+  res.sendFile(__dirname + "/views/capstone.html");
+})
+app.get('/logout', (req, res) => {
+  res.sendFile(__dirname + "/views/login.ejs")
+})
+
+app.post('sign', (req, res) => {
+  res.redirect('/signup');
+})
+
+app.post('/signup', (req, res) => {
+  const email = req.body.Email;
+  const name = req.body.Fullname;
+  db.collection('Database')
+    .where('Email', '==', email).where('Fullname', '==', name)
+    .get()
+    .then((docs) => {
+      if (!docs.empty) {
+
+        res.render('signup.ejs', { data1: 'Email/Username already exists' });
+      } else {
+
+        db.collection('Database')
+          .add({
+            Fullname: name,
+            Email: email,
+            Password: passwordHash.generate(req.body.password)
+          })
+          .then(() => {
+            res.redirect('/login');
+          })
+          .catch((error) => {
+            console.error('Error adding document: ', error);
+            res.render('signup.ejs', { data1: 'Signup failed' });
+
+          });
+      }
+    })
+    .catch(() => {
+
+      res.render('signup.ejs', { data1: 'Signup failed' });
+    });
+});
+
+app.post('/login', (req, res) => {
+  const email = req.body.Email;
+  const password = req.body.password;
+
+  db.collection('Database')
+    .where('Email', '==', email)
+    .get()
+    .then((docs) => {
+      if (!docs.empty) {
+        const user = docs.docs[0].data();
+        const hashedPassword = user.Password;
+
+        if (passwordHash.verify(password, hashedPassword)) {
+          res.redirect('/dashboard');
+        } else {
+          res.render('login.ejs', { data2: 'Invalid Login credentials' });
+        }
+      } else {
+        res.render('login.ejs', { data2: 'Invalid Login credentials' });
+      }
+    })
+    .catch((error) => {
+      console.error('Error retrieving document: ', error);
+      res.render('login.ejs', { data2: 'Login failed' });
+    });
+});
+
+app.post('/logout', (req, res) => {
+  res.redirect('/login')
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
